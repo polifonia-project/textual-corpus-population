f12976bb9a02db1f226713551424edf3f40a1478
Andrea Poltronieri <44606182+andreamust@users.noreply.github.com>
2021-11-16 10:43:48 +0100

    improved error handling

--- image_scraper.py
+++ image_scraper.py
@@ -2,6 +2,7 @@ import urllib.request
 
 import requests
 from bs4 import BeautifulSoup
+from os import listdir
 
 PATH: str = 'https://www.internetculturale.it/it/913/emeroteca-digitale-italiana/periodic/testata/8660'
 
@@ -13,7 +14,9 @@ OUTPUT_PATH: str = 'IlTrovatore'
 
 def get_documents(url: str):
     """
+
     Takes the url of the 'Emeroteca digitale italiana' and returns the list of parsed link to download.
+
     :param url: the base url of a journal on 'Emeroteca digitale italiana'
     :return: a list of links, each of which is the
     """
@@ -28,24 +31,33 @@ def get_documents(url: str):
 
 def download_images(image_link: str, start_resource_url: str, end_resource_url: str, base_path: str = ''):
     """
+
     Takes the url of an image and downloads it into a specific directory.
+
     :param image_link: the url of the image to download
     :param start_resource_url: the start url of the download page of the resource
     :param end_resource_url: the start url of the download page of the resource
     :param base_path: the path where to save the downloaded image
     """
-    # requests.get(f'{XML_START}{image_link}{XML_END}')
     image_url = f'{start_resource_url}{image_link}{end_resource_url}'
     for page in range(1000):
-        image_composed_url = image_url + str(page + 1)
-        file = urllib.request.urlopen(image_composed_url)
-        size = file.headers.get('content-length')
-        if int(size) > 100:
-            with open(f'{base_path}/{image_link.split("ADB")[1]}-{page+1}.jpeg', "wb") as f:
-                f.write(requests.get(image_composed_url).content)
-                print(f'SAVED FILE AS: {base_path}/{image_link.split("ADB")[1]}-{page+1}.jpeg')
+        file_name = f'{base_path}/{image_link.split("ADB")[1]}-{page+1}.jpeg'
+        if file_name.split('/')[1] not in [f for f in listdir(base_path)]:
+            try:
+                image_composed_url = image_url + str(page + 1)
+                file = urllib.request.urlopen(image_composed_url)
+                size = file.headers.get('content-length')
+                if int(size) > 100:
+                    with open(file_name, "wb") as f:
+                        f.write(requests.get(image_composed_url).content)
+                        print(f'SAVED FILE AS: {file_name}')
+                else:
+                    break
+            except:
+                print(f'ERROR ENCOUNTERED WHILE HANDLING THE FILE: {image_link}')
+                pass
         else:
-            break
+            print(f'FILE ALREADY IN DIRECTORY, SKIPPING: {file_name}')
 
 
 if __name__ == '__main__':
