115fd99b5501f6b42c5170ecf5b090bf2ce23deb
Andrea Poltronieri <andrea.poltronieri2@unibo.it>
2021-12-02 19:44:17 +0100

    updated documentation

--- README.md
+++ README.md
@@ -1,3 +1,55 @@
 # Polifonia OCR
 
-This repository contains the code of the OCR experiments for the Polifonia project.
+This repository contains the code for downloading and digitising documents used as a corpus for the [Polifonia Project](https://polifonia-project.eu/).
+The repository contains two main types of code:
+* file scrapers that automate the download of big repositories of textual data;
+* OCR script for digitising the downloaded files. 
+
+Since these two types of scripts can also be used as stand-alone software, they are documented separately, while the setup and the requirement installation is documented for all the scripts contained in the repository.
+
+## Requirements and Installation
+For running all the scripts you need to have Python (3.6+) and pip3 installed on your machine.
+Instructions for installing Python and pip can be found on the [Python download page](https://www.python.org/downloads/).
+
+Once Python is installed, it is necessary to clone the repository using git (installation information on [this page](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git)):
+
+```
+git clone https://github.com/polifonia-project/OCR.git
+```
+
+The libraries needed to execute all scripts can be installed running:
+```
+pip install requirements.txt
+```
+
+For running the OCR Script it is required to install [Tesseract](https://github.com/tesseract-ocr/tesseract) and the trained data for the languages you need to work with.
+The full documentation for installing Tesseract and its dependencies can be found in [Tesseract official documentation](https://tesseract-ocr.github.io/tessdoc/Installation.html).
+
+## Usage
+### File scrapers
+For downloading resources from "Internet Culturale" you need to run the ```internet_culturale_scraper.py``` as:
+```
+python3 internet_culturale_scraper.py [-h] [--resource_url] [--output_path]
+```
+
+The parameter to pass are described as follows:
+```
+--resource_url (string):  the url of a resource page on "Internet Culturale" (e.g. "https://www.internetculturale.it/it/913/emeroteca-digitale-italiana/periodic/testata/8670"
+
+--output_path (string):  the existing path in with to save the downloaded resource
+```
+
+You can also browse the script's documentation by typing:
+```
+python3 internet_culturale_scraper.py --help
+```
+
+The script will download all files related to the given resource to the specified folder. 
+Depending on the size of the resource and the speed of the connection, the download may take several hours.
+
+In addition, a log file named ```download_log.txt``` will be generated in the output folder. This file will list:
+* the number of files downloaded; 
+* the number of errors encountered;
+* the list of files not downloaded.
+
+To attempt to download the non-downloaded files again, simply restart the script with the same parameters. 
--- internet_culturale_scraper.py
+++ internet_culturale_scraper.py
@@ -34,6 +34,9 @@ def download_images(image_link: str, start_resource_url: str, end_resource_url: 
     :param base_path: the path where to save the downloaded image
     """
     image_url = f'{start_resource_url}{image_link}{end_resource_url}'
+    success_count = 0
+    error_count = 0
+    error_list = []
     for page in range(1000):
         # print(image_link)
         file_name = f'{base_path}/{image_link.split("%3")[-1][3:]}-{page+1}.jpeg'
@@ -45,35 +48,59 @@ def download_images(image_link: str, start_resource_url: str, end_resource_url: 
                 if int(size) > 100:
                     with open(file_name, "wb") as f:
                         f.write(requests.get(image_composed_url).content)
+                        success_count += 1
                         print(f'SAVED FILE AS: {file_name}')
                 else:
                     break
             except:
                 print(f'ERROR ENCOUNTERED WHILE HANDLING THE FILE: {image_link}')
+                error_count += 1
+                error_list.append(image_link)
                 pass
         else:
             print(f'FILE ALREADY IN DIRECTORY, SKIPPING: {file_name}')
 
+    return success_count, error_count, error_list
+
+
+def save_log(success_number: int, error_number: int, error_list: list, path: str):
+    with open(f'{path}/download_log.txt', 'w') as f:
+        f.write(f'DOWNLOAD LOG\n\n'
+                f'Successfully downloaded resources: {success_number}\n'
+                f'Errors encountered: {error_number}\n'
+                f'The errors were encountered in the following files:\n')
+        for item in error_list:
+            f.write(f'- {item}\n')
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='Downloads resources from "Internet Culturale".')
 
     parser.add_argument('--resource_url',
                         type=str,
-                        default='https://www.internetculturale.it/it/913/emeroteca-digitale-italiana/periodic/testata'
-                                '/8670')
+                        help='the url of a resource page on "Internet Culturale" (e.g. "https://www.internetculturale.it/it/913/emeroteca-digitale-italiana/periodic/testata/8670"',)
     parser.add_argument('--img_path_start',
                         type=str,
+                        help='the first part of the url string of the image download page (to ba changed only in case of major updates to the website)',
                         default='http://www.internetculturale.it/jmms/objdownload?')
     parser.add_argument('--img_path_end',
                         type=str,
+                        help='the last part of the url string of the image download page (to ba changed only in case of major updates to the website)',
                         default='&teca=Casa%20della%20musica%20di%20Parma&resource=img&mode=raw&start=0&offset=')
     parser.add_argument('--output_path',
                         type=str,
-                        default='/Volumes/MUST/OCR/Arpa')
+                        help='the existing path in with to save the downloaded resource')
 
     args = parser.parse_args()
 
     links = get_documents(args.resource_url)
+    successes = 0
+    errors = 0
+    lists_errors = []
     for link in links:
-        download_images(link, args.img_path_start, args.img_path_end, args.output_path)
+        success, error, list_error = download_images(link, args.img_path_start, args.img_path_end, args.output_path)
+        successes += success
+        errors += error
+        lists_errors.extend(lists_errors)
+
+    save_log(successes, errors, lists_errors, args.output_path)
