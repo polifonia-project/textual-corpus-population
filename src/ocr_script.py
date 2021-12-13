3141325fac00735e0d8b831da26131df06837cc9
roccotrip <rocco.trip@gmail.com>
2021-12-13 22:12:32 +0100

    metadata added

--- src/ocr_script.py
+++ src/ocr_script.py
@@ -1,32 +1,40 @@
-import argparse
 import ntpath
 import os
 import re
-
+import argparse
+#import filetype
 import cv2
 import numpy as np
 import pytesseract
-from PIL import Image
 from pdf2image import convert_from_path
+from PIL import Image
 
 Image.MAX_IMAGE_PIXELS = 933120000
-SUPPORTED_IMAGE_FORMAT = ['.png', '.jpg', '.jpeg', '.tiff', '.gif']
 
 
-def file_info(file_path):
+def file_info(file_path, out_path):
     """Takes as input the file input path and the output path and analyses the file name and the file extension.
     It also set a destination folder for the conversion from pdf to image."""
-    if file_path == "" or file_path is None:
+    if (file_path == "" or file_path is None) and (out_path == "" or out_path is None):
         raise NameError("Directories Not Set")
     elif os.path.isdir(file_path):
+        results_path = file_path
         file_ext = None
         file_name_no_ext = ntpath.basename(file_path)
-    else:
+    elif file_path.endswith(".pdf"):
         file_name = ntpath.basename(file_path)
         file_name_no_ext = os.path.splitext(file_name)[0]
         file_ext = os.path.splitext(file_name)[1]
+        results_path = out_path + file_name_no_ext
+    elif file_path.endswith(".png"):
+        file_name = ntpath.basename(file_path)
+        file_name_no_ext = os.path.splitext(file_name)[0]
+        file_ext = os.path.splitext(file_name)[1]
+        results_path = file_path
+    else:
+        raise NameError("Input File Format Not Valid")
 
-    return file_name_no_ext, file_ext
+    return file_name_no_ext, file_ext, results_path
 
 
 def convert_image(file_path, out_path, out_format="png"):
@@ -35,10 +43,8 @@ def convert_image(file_path, out_path, out_format="png"):
     image.save(out_path)
 
 
-def pdf_to_img(file_path, final_path, out_format='png'):
-    file_name_no_ext, file_ext = file_info(file_path)
-    results_path = f'{final_path}/{file_name_no_ext}'
-    print(file_path)
+def pdf_to_img(file_path, out_path, out_format="png"):
+    file_name_no_ext, file_ext, results_path = file_info(file_path, out_path)
     # check if exists a folder with the same name of the input file. If not, create one.
     if not os.path.isdir(results_path):
         os.makedirs(results_path)
@@ -49,14 +55,11 @@ def pdf_to_img(file_path, final_path, out_format='png'):
     page_num = 0
     for page in pages:
         page_num += 1
-        print(f'SAVING PAGE: {page_num}')
-        page.save(f'{results_path}/{page_num}.{out_format}', out_format)
-    print(results_path)
-    return results_path
+        print("SAVING PAGE: {}".format(page_num))
+        page.save("{}/{}.{}".format(results_path, page_num, out_format), out_format)
 
 
-def image_processing(input_path, gray_scale, remove_noise, thresholding, dilate, erosion, edge_detection,
-                     skew_correction):
+def image_processing(input_path, gray_scale, remove_noise, thresholding, dilate, erosion, edge_detection, skew_correction):
     kernel = np.ones((5, 5), np.uint8)
     image = cv2.imread(input_path)
 
@@ -106,63 +109,36 @@ def ocr(processed_image, language_mode, psm, oem, multilang="", language=""):
     return ocr_output
 
 
-def ocrise_folder(folder_path, saved_file_path, output_format):
-    ocr_all, ocr_all_pdf = '', ''
-    for path, dirs, files in os.walk(folder_path):
-        for file in sorted(files, key=lambda f: int(re.sub('\D', '1', f))):
-            filename, file_extension = os.path.splitext(file)
-            if file_extension == '.pdf':
-                # folder created inside the same folder as the input one.
-                print(f'The file {filename} is a .pdf file. Converting to image in {output_format} format.')
-                converted_image_path = pdf_to_img(f'{path}/{file}', folder_path, output_format)
-                ocrise_pdf(converted_image_path, filename, saved_file_path)
-            elif file_extension in SUPPORTED_IMAGE_FORMAT:
-                text = ocrise_single(input_file=f'{path}/{file}',
-                                     language_mode=args.language_mode,
-                                     single_lang=args.single_language,
-                                     multiple_langs=args.multiple_langs,
-                                     psm=args.page_segmentation_mode,
-                                     oem=args.ocr_engine_mode,
-                                     gray_scale=args.gray_scale,
-                                     remove_noise=args.remove_noise,
-                                     thresholding=args.thresholding,
-                                     dilate=args.dilate,
-                                     erosion=args.erosion,
-                                     edge_detection=args.edge_detection,
-                                     skew_correction=args.skew_correction)
-                if len([x for x in files if '-' in x]) > 0 and len(file.split('-')[:-1]) > 1:
-                    if f"{'-'.join(file.split('-')[:-1])}.txt" not in [f for f in os.listdir(saved_file_path)]:
-                        save_to_txt(f"{saved_file_path}/{'-'.join(file.split('-')[:-1])}.txt", text)
-                    elif f"{'-'.join(file.split('-')[:-1])}.txt" in [f for f in os.listdir(saved_file_path)]:
-                        with open(f"{saved_file_path}/{'-'.join(file.split('-')[:-1])}.txt", "a") as existing_file:
-                            existing_file.write(f"\n\n\n{text}")
+def ocrise_multiple(final_path, language_mode, single_lang, multiple_langs, output_format, psm, oem,
+                    gray_scale, remove_noise, thresholding, dilate, erosion, edge_detection, skew_correction):
+    ocr_all = ""
+    print("PROCESSING FOLDER: {}".format(final_path))
+    for path, dirs, images in os.walk(final_path):
+        for image in sorted(images, key=lambda f: int(re.sub('\D', '1', f))):
+            filename, file_extension = os.path.splitext(image)
+            if file_extension == ".{}".format(output_format):
+                print("PROCESSING IMAGE: {}/{}".format(path, image))
+
+                processed_image = image_processing("{}/{}".format(path, image), gray_scale, remove_noise, thresholding,
+                                                   dilate, erosion, edge_detection, skew_correction)
+                image_ocr = ocr(processed_image, language_mode, psm, oem,
+                                multiple_langs, single_lang)
+                if len(filename.split('-')[:-1]) > 1:
+                    if extension is None and f"{'-'.join(filename.split('-')[:-1])}.txt" not in [f for f in
+                                                                                                 os.listdir('../')]:
+                        save_to_txt(f"{'-'.join(filename.split('-')[:-1])}.txt", image_ocr)
+                    elif extension is None and f"{'-'.join(filename.split('-')[:-1])}.txt" in [f for f in
+                                                                                               os.listdir('../')]:
+                        with open(f"{'-'.join(filename.split('-')[:-1])}.txt", "a") as existing_file:
+                            existing_file.write(f"\n\n\n{image_ocr}")
                 else:
-                    ocr_all = ocr_all + text
+                    ocr_all = ocr_all + image_ocr
+                if extension is not None:
+                    ocr_all = ocr_all + image_ocr
             else:
-                print(f'FILE FORMAT NOT SUPPORTED (yet!), SKIPPING {file}')
-        if len(ocr_all) > 0:
-            save_to_txt(f'{saved_file_path}/{path.split("/")[-1]}.txt', ocr_all)
-
-
-def ocrise_pdf(converted_image_path, filename, output_folder):
-    ocr_all_pdf = ''
-    for path, dirs, files in os.walk(converted_image_path):
-        for img in sorted(files, key=lambda f: int(re.sub('\D', '1', f))):
-            text = ocrise_single(input_file=f'{path}/{img}',
-                                 language_mode=args.language_mode,
-                                 single_lang=args.single_language,
-                                 multiple_langs=args.multiple_langs,
-                                 psm=args.page_segmentation_mode,
-                                 oem=args.ocr_engine_mode,
-                                 gray_scale=args.gray_scale,
-                                 remove_noise=args.remove_noise,
-                                 thresholding=args.thresholding,
-                                 dilate=args.dilate,
-                                 erosion=args.erosion,
-                                 edge_detection=args.edge_detection,
-                                 skew_correction=args.skew_correction)
-            ocr_all_pdf = ocr_all_pdf + text
-    save_to_txt(f'{output_folder}/{filename}.txt', ocr_all_pdf)
+                print("UNABLE TO PROCESS FILE: {}".format(image))
+                continue
+    return ocr_all
 
 
 def ocrise_single(input_file, language_mode, single_lang, multiple_langs, psm, oem,
@@ -188,16 +164,16 @@ if __name__ == "__main__":
     # File parameters
     parser.add_argument('--input_path',
                         type=str,
-                        default='/Users/andreapoltronieri/PycharmProjects/ocr/test_pdf/cde.pdf')  # accepts pdf files, image files and image folders
-    parser.add_argument('--saved_file_path',
+                        default='/Users/andreapoltronieri/PycharmProjects/ocr/Evo2022_paper_186')  # accepts pdf files, image files and image folders
+    parser.add_argument('--output_path',
                         type=str,
-                        default='/Users/andreapoltronieri/PycharmProjects/ocr/result/')  # only needed if the input format is pdf
-    parser.add_argument('--converted_image_output_path',
-                        type=str,
-                        default='/Users/andreapoltronieri/PycharmProjects/ocr/test_pdf/')  # only needed if the input format is pdf
+                        default='')  # only needed if the input format is pdf
     parser.add_argument('--output_format',
                         type=str,
                         default='png')  # only needed if the input format is pdf
+    parser.add_argument('--output_name',
+                        type=str,
+                        default='test2.txt')  # name of the output .txt file
 
     # Language parameters
     parser.add_argument('--language_mode',
@@ -225,26 +201,48 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    file_name, extension = file_info(args.input_path)
-    if extension == ".pdf" and not os.path.isdir(args.input_path):
+    file_name, extension, final_path = file_info(args.input_path, args.output_path)
+    if extension == ".pdf" and not os.path.isdir(final_path):
         print("The input file is a .pdf file. Converting to image in {} format.".format(args.output_format))
-        converted_images = pdf_to_img(args.input_path, args.converted_image_output_path, args.output_format)
-        ocrise_pdf(converted_images, file_name, args.saved_file_path)
-    elif os.path.isdir(args.input_path):
+        pdf_to_img(args.input_path, args.output_path, args.output_format)
+
+    elif extension is None:
         print("The input corresponds to a folder. Processing files contained in it.")
-        ocrise_folder(args.input_path, args.saved_file_path, args.output_format)
+        args.output_path = final_path
     else:
-        text = ocrise_single(input_file=args.input_path,
-                             language_mode=args.language_mode,
-                             single_lang=args.single_language,
-                             multiple_langs=args.multiple_langs,
-                             psm=args.page_segmentation_mode,
-                             oem=args.ocr_engine_mode,
-                             gray_scale=args.gray_scale,
-                             remove_noise=args.remove_noise,
-                             thresholding=args.thresholding,
-                             dilate=args.dilate,
-                             erosion=args.erosion,
-                             edge_detection=args.edge_detection,
-                             skew_correction=args.skew_correction)
-        save_to_txt(f'{args.saved_file_path}/{file_name}.txt', text)
+        pass
+
+    ocr_all = ""
+    if extension is None or extension == ".pdf":
+        ocr_all = ocrise_multiple(final_path=args.output_path,
+                                  language_mode=args.language_mode,
+                                  single_lang=args.single_language,
+                                  multiple_langs=args.multiple_langs,
+                                  output_format=args.output_format,
+                                  psm=args.page_segmentation_mode,
+                                  oem=args.ocr_engine_mode,
+                                  gray_scale=args.gray_scale,
+                                  remove_noise=args.remove_noise,
+                                  thresholding=args.thresholding,
+                                  dilate=args.dilate,
+                                  erosion=args.erosion,
+                                  edge_detection=args.edge_detection,
+                                  skew_correction=args.skew_correction)
+    else:
+        ocr_all = ocrise_single(input_file=args.input_path,
+                                language_mode=args.language_mode,
+                                single_lang=args.single_language,
+                                multiple_langs=args.multiple_langs,
+                                psm=args.page_segmentation_mode,
+                                oem=args.ocr_engine_mode,
+                                gray_scale=args.gray_scale,
+                                remove_noise=args.remove_noise,
+                                thresholding=args.thresholding,
+                                dilate=args.dilate,
+                                erosion=args.erosion,
+                                edge_detection=args.edge_detection,
+                                skew_correction=args.skew_correction)
+
+    if len(ocr_all) > 1:
+        save_to_txt(args.output_name, ocr_all)
+        print("\nOCR completed.\n\nSaved file as: {}".format(args.output_name))
