da6bd5a20ef49a4494f9463f67dc28fde2f1bb5a
arianna-graciotti <arianna.graciotti@gmail.com>
2022-04-30 15:47:54 +0200

    v0.7 Improved to manage sources coming from dbnl.

--- src/download_all_NL_220422_AG.py
+++ src/download_all_NL_220422_AG.py
@@ -1,19 +1,9 @@
-import argparse
-import csv
 import os
-import urllib
-from os import listdir
-import pandas as pd
-import csv
-
 import requests
-from bs4 import BeautifulSoup
-
-import time
 
 TIMEOUT = 20
 
-download_path = '/Users/arianna/Desktop/Polifonia/WP4/D4_2/prove_download/delpher_books_nl_300922/'
+download_path = '/Users/arianna/Desktop/Polifonia/WP4/D4_2/prove_download/delpher_books_nl_300922_5/'
 
 from_tsv_to_crawling = []
 with open("from_tsv_to_crawling_input.tsv") as file:
@@ -23,7 +13,12 @@ with open("from_tsv_to_crawling_input.tsv") as file:
 
 for persistent_url in from_tsv_to_crawling:
     resource_id = persistent_url[0].replace(':', '_')
-    download_url = persistent_url[1][:-1]
+    download_url = None
+    if 'dbnl' in resource_id:
+        persistent_url_dbnl = persistent_url[0].split("=")[-1].split('_')[-1]
+        download_url = 'https://www.dbnl.org/tekst/' + persistent_url_dbnl + '_01' + '/' + persistent_url_dbnl + '_01.pdf'
+    else:
+        download_url = persistent_url[1][:-1]
     if download_url:
         if f'{resource_id}.pdf' in os.listdir(download_path):
             print(f'FILE ALREADY IN DIRECTORY, SKIPPING {download_url}')
